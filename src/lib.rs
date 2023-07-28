f1c53969c993577e2e475f53810d12911ba6909b
Takumi Kobayashi <t.kobayashi@inside.tworks.co.jp>
2023-07-28 16:43:35 +0900

    prelude fixed.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,6 @@ pub mod post;
 pub mod prelude;
 
 #[async_trait::async_trait]
-trait Executor<T> {
+pub trait Executor<T> {
     async fn execute(&self, client: &client::Client) -> Result<T, Box<dyn std::error::Error>>;
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -2,3 +2,4 @@ pub use crate::client::*;
 pub use crate::config::*;
 pub use crate::error;
 pub use crate::post;
+pub use crate::Executor;
